8a3655822b6352c5ef2a66bb16d3ddf2a7fe7e13
Drax54 <dhcp73@gmail.com>
2023-07-19 20:46:47 +0100

    first commit

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+
+
+
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# set chrome options
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
 driver.get("https://medium.com")
 print(driver.page_source)
 print("Finished!")
