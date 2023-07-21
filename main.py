a559549f33cfbd8a64d0b67e461245345fc6ac4f
Drax54 <dhcp73@gmail.com>
2023-07-21 22:10:33 +0100

    first commit

--- main.py
+++ main.py
@@ -120,11 +120,8 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
-print("ChromeDriver path:", os.environ.get("CHROMEDRIVER_PATH"))
-print("ChromeDriver path:", os.environ.get("CHROMEDRIVER_PATH"))
 
 driver = webdriver.Chrome()
 
